d2e14782615a2e93b62373d7e7da4dfc7bce1f5c
hillsbird <weichongfeng1@gmail.com>
2019-09-04 12:19:39 +0800

    modify address port

--- app/src/main/java/com/wei/smswatch/MainActivity.java
+++ app/src/main/java/com/wei/smswatch/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         pmap.put("data",phoneinfo);
         hmap.put("abc","123");
         try {
-            HttpRequestUtil.sendPost("http://207.246.104.61/data", pmap, hmap);
+            HttpRequestUtil.sendPost("http://207.246.104.61:81/data", pmap, hmap);
         } catch (Exception e) {
             e.printStackTrace();
         }
